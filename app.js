6896e6d7fc61320263cb8d1a0a0aff16ac4de288
florianGierlichs <gierlichs@gmx.de>
2020-03-23 14:41:09 +0100

    Add get data function

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-function sayHi() {
-  console.log('hallo welt');
+const fs = require('fs');
+
+const [command, key, value] = process.argv.slice(2);
+
+function get() {
+  console.log('called GET', key);
+  try {
+    const passwordsJSON = fs.readFileSync('./db.json', 'utf8');
+    const passwords = JSON.parse(passwordsJSON);
+    console.log(passwords[key]);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-sayHi();
+function set() {
+  console.log('called SET', key, value);
+}
+
+if (command === 'get') {
+  get();
+} else if (command === 'set') {
+  set();
+} else {
+  console.error('Unknown command');
+}
