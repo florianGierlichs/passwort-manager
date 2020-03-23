b1f9eb257f03ba82ed7151d9a348cff794d75d20
florianGierlichs <gierlichs@gmx.de>
2020-03-23 17:49:48 +0100

    Outsource command functions

--- app.js
+++ app.js
@@ -1,55 +1,14 @@
-// const passwords = require('./lib/passwords');
-// const readPasswords = passwords.readPasswords;
-// const writePasswords = passwords.writePasswords;
 const { readPasswords, writePasswords } = require('./lib/passwords');
+const { get, set, unset } = require('./lib/commands');
 
 const [command, key, value] = process.argv.slice(2);
 
-function get() {
-  console.log('called GET', key);
-  try {
-    const passwords = readPasswords();
-
-    console.log(passwords[key]);
-    return passwords;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function set() {
-  console.log('called SET', key, value);
-
-  try {
-    const passwords = readPasswords();
-    passwords[key] = value;
-    console.log(passwords[key]);
-
-    writePasswords(passwords);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function unset() {
-  console.log('called UNSET', key);
-  try {
-    const passwords = readPasswords();
-    delete passwords[key];
-    console.log(passwords[key]);
-
-    writePasswords(passwords);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 if (command === 'get') {
-  get();
+  get(key, readPasswords);
 } else if (command === 'set') {
-  set();
+  set(key, value, readPasswords, writePasswords);
 } else if (command === 'unset') {
-  unset();
+  unset(key, readPasswords, writePasswords);
 } else {
   console.error('Unknown command');
 }
