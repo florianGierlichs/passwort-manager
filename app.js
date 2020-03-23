95ae67f78be888c5ad3f7f3194e6af85b394e25c
florianGierlichs <gierlichs@gmx.de>
2020-03-23 15:43:32 +0100

    Set password

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ function get() {
   try {
     const passwordsJSON = fs.readFileSync('./db.json', 'utf8');
     const passwords = JSON.parse(passwordsJSON);
+
     console.log(passwords[key]);
+    return passwords;
   } catch (error) {
     console.error(error);
   }
@@ -15,6 +17,19 @@ function get() {
 
 function set() {
   console.log('called SET', key, value);
+  // Read db.json
+  // Update value by key
+  // Write db.json
+  try {
+    const passwordsJSON = fs.readFileSync('db.json', 'utf8');
+    const passwords = JSON.parse(passwordsJSON);
+    passwords[key] = value;
+    console.log(passwords[key]);
+
+    fs.writeFileSync('db.json', JSON.stringify(passwords, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 if (command === 'get') {
